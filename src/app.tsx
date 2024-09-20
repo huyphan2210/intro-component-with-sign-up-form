33cd23d576b0f70956d27825671be21b9122ea9c
Huy Phan <huy.phan2210@outlook.com>
2024-09-20 11:41:06 +0700

    Update app + create basic structure

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,15 @@
 import "./app.scss";
 
 export function App() {
-  return <></>;
+  const heading = "Learn to code by watching others";
+  return (
+    <main>
+      <h1>{heading}</h1>
+      <form>
+        <section className="fields"></section>
+        <section className="terms"></section>
+        <section className="button-group"></section>
+      </form>
+    </main>
+  );
 }
