6f3a47c97443b03a123f6640c3d7cdcdd50296a2
Huy Phan <huy.phan2210@outlook.com>
2024-09-24 09:51:28 +0700

    Remove contentText, use children instead

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,12 +2,18 @@ import { FC } from "preact/compat";
 import "./Button.scss";
 
 interface ButtonProps {
-  contentText: string;
   isForSubmitting: boolean;
 }
 
-const Button: FC<ButtonProps> = () => {
-  return <button></button>;
+const Button: FC<ButtonProps> = ({ isForSubmitting, children }) => {
+  return (
+    <button
+      className={`btn${isForSubmitting ? " submit" : " link"}`}
+      type={isForSubmitting ? "submit" : "button"}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
