5c6e49d01a1b348cc1e07203c34bf86229f8d1b4
Huy Phan <huy.phan2210@outlook.com>
2024-09-26 11:41:11 +0700

    Add termList + use Checkbox component

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,12 @@
 import "./app.scss";
 import Button from "./components/Button/Button";
+import Checkbox, { CheckboxProps } from "./components/Checkbox/Checkbox";
 import Input from "./components/Input/Input";
+import { ReactElement } from "preact/compat";
+
+interface Condition extends CheckboxProps {
+  content: ReactElement;
+}
 
 export function App() {
   const heading = "Learn to code by watching others";
@@ -10,17 +16,70 @@ export function App() {
     e.preventDefault();
   };
 
+  const conditionList: Condition[] = [
+    {
+      label: "terms",
+      content: (
+        <>
+          I agree to the{" "}
+          <a
+            href="https://termly.io/resources/templates/terms-of-service-template/"
+            target="_blank"
+          >
+            Terms and Services
+          </a>{" "}
+          of <span className="brand-name">CodeBlue</span>
+        </>
+      ),
+    },
+    {
+      label: "newsletter",
+      content: (
+        <>
+          Send me the{" "}
+          <a
+            href="https://termly.io/resources/templates/terms-of-service-template/"
+            target="_blank"
+          >
+            Newsletter
+          </a>{" "}
+          of <span className="brand-name">CodeBlue</span>
+        </>
+      ),
+    },
+  ];
+
   return (
     <main>
       <h1>{heading}</h1>
       <form onSubmit={handleSubmit}>
         <section className="input-group">
-          <Input labelText="First Name" inputType="text" placeholderText="John" />
+          <Input
+            labelText="First Name"
+            inputType="text"
+            placeholderText="John"
+          />
           <Input labelText="Last Name" inputType="text" placeholderText="Doe" />
-          <Input labelText="Email" inputType="email" placeholderText="johndoe@yopmail.com" />
-          <Input labelText="Password" inputType="password" placeholderText="***" />
+          <Input
+            labelText="Email"
+            inputType="email"
+            placeholderText="johndoe@yopmail.com"
+          />
+          <Input
+            labelText="Password"
+            inputType="password"
+            placeholderText="***"
+          />
         </section>
-        <section className="terms"></section>
+        <section className="term-group">
+          <ul>
+            {conditionList.map(({ label, content }) => (
+              <li>
+                <Checkbox label={label}>{content}</Checkbox>
+              </li>
+            ))}
+          </ul>
+        </section>
         <section className="btn-group">
           <Button isForSubmitting>{submitBtnContent}</Button>
           <span>or</span>
