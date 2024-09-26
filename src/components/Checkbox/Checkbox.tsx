fa88cac712b116e358beec615ab31f4276c2d3fd
Huy Phan <huy.phan2210@outlook.com>
2024-09-26 11:40:35 +0700

    Create Checkbox

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,37 @@
+import { FC, useState } from "preact/compat";
+import "./Checkbox.scss";
+
+import checkmark from "../../assets/icons/checkmark.svg";
+
+export interface CheckboxProps {
+  label: string;
+}
+
+const Checkbox: FC<CheckboxProps> = ({ label, children }) => {
+  const [checked, setChecked] = useState(false);
+  const handleCheckbox = () => {
+    const checkbox = document.getElementById(label) as HTMLInputElement;
+    setChecked(checkbox.checked);
+  };
+
+  const handleCustomCheckbox = () => {
+    const checkbox = document.getElementById(label) as HTMLInputElement;
+    checkbox.checked = !checkbox.checked;
+    setChecked(checkbox.checked);
+  };
+
+  return (
+    <div className="checkbox-group">
+      <input id={label} type="checkbox" onChange={handleCheckbox} />
+      <button
+        onClick={handleCustomCheckbox}
+        className={`checkbox${checked ? " checked" : ""}`}
+      >
+        <img src={checkmark} loading="lazy" alt="Checkmark" />
+      </button>
+      <label htmlFor={label}>{children}</label>
+    </div>
+  );
+};
+
+export default Checkbox;
