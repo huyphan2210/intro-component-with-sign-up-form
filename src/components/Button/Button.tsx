2a8915e0f89644f958aa31311ef5cfd978c84910
Huy Phan <huy.phan2210@outlook.com>
2024-09-30 11:24:38 +0700

    Handle disabled state of buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,13 +3,15 @@ import "./Button.scss";
 
 interface ButtonProps {
   isForSubmitting: boolean;
+  isDisabled: boolean;
 }
 
-const Button: FC<ButtonProps> = ({ isForSubmitting, children }) => {
+const Button: FC<ButtonProps> = ({ isForSubmitting, isDisabled, children }) => {
   return (
     <button
       className={`btn${isForSubmitting ? " submit" : " link"}`}
       type={isForSubmitting ? "submit" : "button"}
+      disabled={isDisabled}
     >
       {children}
     </button>
