fe519502c65e2119bc8b6976f8193cfe22303c64
Huy Phan <huy.phan2210@outlook.com>
2024-10-02 11:12:55 +0700

    Use Background component

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,9 @@ import "./app.scss";
 import Button from "./components/Button/Button";
 import Checkbox, { CheckboxProps } from "./components/Checkbox/Checkbox";
 import Input from "./components/Input/Input";
+import codeIcon from "./assets/icons/code-background-1.svg";
 import { ReactElement } from "preact/compat";
+import Background from "./components/Background/Background";
 
 interface Condition extends CheckboxProps {
   content: ReactElement;
@@ -52,49 +54,58 @@ export function App() {
   ];
 
   return (
-    <main>
-      <hgroup>
-        <h1>{heading}</h1>
-        <p>{paragraph}</p>
-      </hgroup>
-      <form onSubmit={handleSubmit}>
-        <section className="input-group">
-          <Input
-            labelText="First Name"
-            inputType="text"
-            placeholderText="John"
-          />
-          <Input labelText="Last Name" inputType="text" placeholderText="Doe" />
-          <Input
-            labelText="Email"
-            inputType="email"
-            placeholderText="johndoe@yopmail.com"
-          />
-          <Input
-            labelText="Password"
-            inputType="password"
-            placeholderText="***"
-          />
-        </section>
-        <section className="term-group">
-          <ul>
-            {conditionList.map(({ label, content }) => (
-              <li>
-                <Checkbox label={label}>{content}</Checkbox>
-              </li>
-            ))}
-          </ul>
-        </section>
-        <section className="btn-group">
-          <Button isForSubmitting isDisabled>{submitBtnContent}</Button>
-          <span>or</span>
-          <Button isForSubmitting={false} isDisabled={false}>
-            <a href="https://huy-phan-portfolio.netlify.app/" target="_blank">
-              Try it free for 7 days
-            </a>
-          </Button>
-        </section>
-      </form>
-    </main>
+    <>
+      <Background />
+      <main>
+        <hgroup>
+          <h1>{heading}</h1>
+          <p>{paragraph}</p>
+        </hgroup>
+        <form onSubmit={handleSubmit}>
+          <section className="input-group">
+            <Input
+              labelText="First Name"
+              inputType="text"
+              placeholderText="John"
+            />
+            <Input
+              labelText="Last Name"
+              inputType="text"
+              placeholderText="Doe"
+            />
+            <Input
+              labelText="Email"
+              inputType="email"
+              placeholderText="johndoe@yopmail.com"
+            />
+            <Input
+              labelText="Password"
+              inputType="password"
+              placeholderText="***"
+            />
+          </section>
+          <section className="term-group">
+            <ul>
+              {conditionList.map(({ label, content }) => (
+                <li>
+                  <Checkbox label={label}>{content}</Checkbox>
+                </li>
+              ))}
+            </ul>
+          </section>
+          <section className="btn-group">
+            <Button isForSubmitting isDisabled>
+              {submitBtnContent}
+            </Button>
+            <span>or</span>
+            <Button isForSubmitting={false} isDisabled={false}>
+              <a href="https://huy-phan-portfolio.netlify.app/" target="_blank">
+                Try it free for 7 days
+              </a>
+            </Button>
+          </section>
+        </form>
+      </main>
+    </>
   );
 }
