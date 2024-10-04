c0be9836e52d7a802684eef6bc462875b4c64445
Huy Phan <huy.phan2210@outlook.com>
2024-10-04 12:13:55 +0700

    Handle validation + add tooltip for input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,10 @@
-import { FC, HTMLInputTypeAttribute } from "preact/compat";
+import {
+  ChangeEvent,
+  FC,
+  HTMLInputTypeAttribute,
+  JSX,
+  useState,
+} from "preact/compat";
 import "./Input.scss";
 
 import emailSvg from "../../assets/icons/email.svg";
@@ -10,11 +16,47 @@ interface InputProps {
   inputType: HTMLInputTypeAttribute;
 }
 
+const errorTextRecords: Record<string, string> = {
+  email: "The email is invalid",
+  password: "The password is invalid",
+  text: "This field requires text only",
+};
+
+const toolTipContent: Record<string, JSX.Element> = {
+  email: <></>,
+  password: <></>,
+  text: <span>This field allows text only</span>,
+};
+
 const Input: FC<InputProps> = ({ inputType, labelText, placeholderText }) => {
+  const [isValid, setIsValid] = useState(true);
+
+  const regexHandler = (regex: RegExp, value: string) => {
+    setIsValid(regex.test(value));
+  };
+
+  const validationHandler: Record<string, (value: string) => void> = {
+    email: (value: string) => {
+      regexHandler(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, value);
+    },
+    password: (value: string) => {
+      regexHandler(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        value
+      );
+    },
+    text: (value: string) => {
+      regexHandler(/^[A-Za-z]+$/, value);
+    },
+  };
+
   return (
-    <div className="input-component">
+    <div className={`input-component${!isValid ? " error" : ""}`}>
       <div className="label-group">
         <label htmlFor={labelText}>{labelText}</label>
+        {!isValid && (
+          <label htmlFor={labelText}>{errorTextRecords[inputType]}</label>
+        )}
       </div>
       <div className="input">
         <input
@@ -22,6 +64,10 @@ const Input: FC<InputProps> = ({ inputType, labelText, placeholderText }) => {
           type={inputType}
           placeholder={placeholderText}
           autoComplete={inputType === "email" ? "current-password" : ""}
+          onChange={(e: ChangeEvent) => {
+            const input = e.currentTarget as HTMLInputElement;
+            validationHandler[input.type](input.value);
+          }}
         />
         {inputType === "email" && (
           <img
@@ -40,6 +86,7 @@ const Input: FC<InputProps> = ({ inputType, labelText, placeholderText }) => {
           />
         )}
       </div>
+      <div className="input-tooltip">{toolTipContent[inputType]}</div>
     </div>
   );
 };
